caec7366e38d1a35e58999322db0c93e55379da9
davidclarra <54649572+davidclarra@users.noreply.github.com>
2019-09-04 21:09:24 -0300

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -3,9 +3,19 @@
 
 //var_dump($_GET); // toma los valores ingresados y nos da sus atributos (string, float, int)
 
-//echo "Su nombre es ".$_GET['nombre'];
+//echo "Su nombre es ".$_GET['nombre']; //trae el nombre ingresado
 
-$archivo = fopen('usuario.txt', 'a');
-	fwrite($archivo, $_GET['nombre']."\n");
+$archivo=fopen('usuario.txt', 'a'); //crea un archivo usuario.txt 'a' abre el archivo e ingresa datos
+	
+	fwrite($archivo, $_GET['nombre']."\n"); //ingresa nombre dentro del archivo y agrega una linea
+
+	fclose($archivo);
+
+$miobjeto = new stdClass();
+$miobjeto->nombre="David";
+$miobjeto->apellido="Clarra";
+
+var_dump($miobjeto);
+
 
 ?>
--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 <h2>HTML Forms</h2>
 
 <form action="action_page.php">
-  nombre:<br>
+  Nombre:<br>
   <input type="text" name="nombre" value="">
   <br>
-  apellido:<br>
+  Apellido:<br>
   <input type="text" name="apellido" value="">
   <br><br>
-  <input type="submit" value="Submit">
+  <input type="submit" value="Enviar">
 </form> 
 
-<p>If you click the "Submit" button, the form-data will be sent to a page called "action_page.php".</p>
+<p>Si usted clickea en el boton "Enviar", los datos del formulario serán enviados a la pagina llamada "action_page.php".</p>
 
 </body>
 </html>
