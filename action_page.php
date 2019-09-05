a91e79aec46fe3f1bde2021b4af339d608e7611b
davidclarra <54649572+davidclarra@users.noreply.github.com>
2019-09-04 21:20:57 -0300

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -5,17 +5,18 @@
 
 //echo "Su nombre es ".$_GET['nombre']; //trae el nombre ingresado
 
+$miobjeto = new stdClass();
+$miobjeto->nombre=$_GET['nombre'];
+$miobjeto->apellido=$_GET['apellido'];
+var_dump(json_encode($miobjeto));
+
 $archivo=fopen('usuario.txt', 'a'); //crea un archivo usuario.txt 'a' abre el archivo e ingresa datos
 	
-	fwrite($archivo, $_GET['nombre']."\n"); //ingresa nombre dentro del archivo y agrega una linea
+	fwrite($archivo,json_encode($miobjeto)."\n");
+
+	//fwrite($archivo, $_GET['nombre']."\n"); //ingresa nombre dentro del archivo y agrega una linea
 
 	fclose($archivo);
-
-$miobjeto = new stdClass();
-$miobjeto->nombre="David";
-$miobjeto->apellido="Clarra";
-
-var_dump($miobjeto);
 
 
 ?>
